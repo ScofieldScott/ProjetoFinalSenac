de3efbe6a8621a526b343101b1437e59fe06f91f
pedrocatelli <pedrocatelli.cic@gmail.com>
2024-09-18 21:54:18 -0300

    Alterações no HTML e Js

--- js/funcoes.js
+++ js/funcoes.js
@@ -5,12 +5,16 @@ function listarCombos() {
             let todosOsCombos = document.getElementById("combos")
             todosOsCombos.innerHTML = ""
             dados.forEach(function (dado) {
-                let conteudo = `<a href="Combo.html?idCombo=${dado.id}">
+                let conteudo = `<div class="related-box">
+                <a href="Combo.html?idCombo=${dado.id}">
                                         <img class="box-de-comida--1" src="${dado.img64}" alt="">
-                                        <span>${dado.nome}</span>
-                                        </br>
-                                        <span>${dado.descricao}</span>
-                                    </a>`
+                                        <div>
+                                            <span>${dado.nome}</span>
+                                            <p>${dado.descricao}</p>
+                                            
+                                        </div>
+                                    </a>
+                                    </div>`
 
 
                 todosOsCombos.innerHTML = todosOsCombos.innerHTML + conteudo;
@@ -19,7 +23,7 @@ function listarCombos() {
         );
 }
 
-function recuperarCombo(id) {
+function recuperarCombo() {
 
     let url = new URL(window.location.href);
     //recuperando o id da pessoa que esta no parametro de nome idPessoa
@@ -44,8 +48,8 @@ function recuperarCombo(id) {
                 <strong class="preco">${"R$ " + dados.preco}</strong>
                 
                 <div class="adicionar-carrinho">
-                    <a href="detalhesDoPedido.html">
-                        <button>Adicionar</button>
+                    <a href="detalhesDoPedido.html?idCombo=${idCombo}">
+                        <button>Escolher marmitas</button>
                     </a>
                 </div>
             </div>`
@@ -61,7 +65,9 @@ function listarMarmitas() {
             let todasAsMarmitas = document.getElementById("marmitas")
             todasAsMarmitas.innerHTML = ""
             dados.forEach(function (dado) {
-                    let conteudo = `<a class="related-post" href="MarmitaDeComida.html?idMarmita=${dado.id}">
+                    let conteudo = `
+                    <div class="related-box">
+                    <a class="related-post" href="MarmitaDeComida.html?idMarmita=${dado.id}">
                                 <img class="box-de-comida--1" src="${dado.proteina.img64}" alt="">
 
                                 <div class="descricao">
@@ -70,7 +76,8 @@ function listarMarmitas() {
                                         ${dado.acompanhamentos}
                                     </p>
                                 </div>
-                            </a>`
+                            </a>
+                    </div>`
                             
                 todasAsMarmitas.innerHTML = todasAsMarmitas.innerHTML + conteudo;
             })
@@ -105,70 +112,6 @@ function recuperarMarmita(id) {
         })
 }
 
-//Continuação PAULO
-
-function listarCarrinho(id) {
-    fetch('http://localhost:8080/carrinho/' + id).then(response => response.json()).
-        then(function (dados) {
-            let carrinho = document.getElementById("dadosCarrinho")
-            carrinho.innerHTML = ""
-            dados.forEach(function (dado) {
-                let conteudo = `<img src="${dado.img64}" alt="">
-            <div class="dados-carrinho">
-                <h3>
-                    <strong>${dado.nome}</strong>
-                </h3>
-                <p>
-                    ${dado.descricao}
-                </p>
-                <strong class="preco">${dado.preco}</strong>
-                <a href="#" onclick="removerItem(${dado.id})">
-                    Remover
-                </a>
-            </div>`
-
-                carrinho.innerHTML = carrinho.innerHTML + conteudo;
-            })
-        }
-        );
-}
-
-function recuperaCarrinho() {
-    let url = new URL(window.location.href);
-    //recuperando o id da pessoa que esta no parametro de nome idPessoa
-    let idPessoa = url.searchParams.get('idPessoa');
-    fetch('http://localhost:8080/prato').
-        then(response => response.json()).
-        then(function (dados) {
-            let carrinho = document.getElementById('dadosCarrinho');
-            carrinho.innerHTML = ""
-            let carro = `
-        
-        <div class="carrinho-cheio">
-            <img src="${dados.img64}" alt="">
-            <h4>
-                ${dados.nome}
-            </h4>
-
-            <div class="valor-produto" id="valor-produto">
-                    <span class="quantidade">${dados.quantidade}</span>
-                    <button class="contador">
-                    </button>
-
-                    <div class="preco">
-
-                        <span>${"R$ " + dados.preco}</span>
-                    </div>
-
-                </div>
-
-            </div>`
-
-            carro.innerHTML = carro.innerHTML + conteudo;
-
-
-        });
-
 //listar marmitas mais Pedidos gustavo
 function listarMarmitasMaisPedidos(){
     fetch('http://localhost:8080/MarmitasMaisPedidos').
@@ -188,88 +131,14 @@ function listarMarmitasMaisPedidos(){
     });
 }
 
-function recuperarMarmitasMaisPedidos(id){
-    fetch('http://localhost:8080/MarmitasMaisPedidos/').then(response =>response.json()).
-    then(function (dados){
-        let MarmitasMaisPedidos = document.getElementById("dadosMarmitasMaisPedidos")
-        MarmitasMaisPedidos.innerHTML = ""
-
-        let conteudo = `<img src="${dados.img64}" alt="">
-
-            <div class="detalhes-do-prato">
-                <h3>
-                    <strong>${dados.nome}</strong>
-                </h3>
-
-                <p>
-                    ${dados.descricao}
-                </p>
-
-                <strong class="preco">${dados.preco}</strong>
-                
-                <div class="adicionar-carrinho">
-                    <a href="detalhesDoPedidoPratos.html">
-                        <button>Adicionar</button>
-                    </a>
-                </div>
-            </div>`
-
-        MarmitasMaisPedidos.innerHTML = MarmitasMaisPedidos.innerHTML + conteudo;
-    })
-}
-
-//listar marmitas
-function listarMarmitas(){
-    fetch('http://localhost:8080/Marmitas').
-    then(response=>response.json()).
-    then(function(dados){
-        let todosOsMarmitas = document.getElementById("marmitas")
-        todosOsMarmitas.innerHTML=""
-        dados.forEach(function(dad){
-            let conteudo =`href=menuMarmitas.html".html?idMarmitas=${dad.id}">
-            <span>${dad.nome}</span>
-            </br>
-            <span>${dad.descricao}</span>
-            </a>`
-
-        todosOsMarmitas.innerHTML = todosOsMarmitas.innerHTML + conteudo;
-        });
-    });
-}
-
-function recuperarMarmitas(id){
-    fetch('http://localhost:8080/Marmitas/').then(response =>response.json()).
-    then(function (dados){
-        let Marmitas = document.getElementById("dadosMarmitas")
-        Marmitas.innerHTML = ""
-
-        let conteudo = `<img src="${dados.img64}" alt="">
-
-            <div class="detalhes-do-prato">
-                <h3>
-                    <strong>${dados.nome}</strong>
-                </h3>
-
-                <p>
-                    ${dados.descricao}
-                </p>
-
-                <strong class="preco">${dados.preco}</strong>
-                
-                <div class="adicionar-carrinho">
-                    <a href="detalhesDoPedidoPratos.html">
-                        <button>Adicionar</button>
-                    </a>
-                </div>
-            </div>`
-
-        Marmitas.innerHTML = Marmitas.innerHTML + conteudo;
-    })
-}
-}
 
 function listarMarmitasDoCombo() {
-    fetch('http://localhost:8080/marmita').
+
+    let url = new URL(window.location.href);
+    
+    let idCombo = url.searchParams.get('idCombo');
+
+    fetch('http://localhost:8080/marmita/combo/'  + idCombo).
         then(response => response.json()).
         then(function (dados) {
             let todasAsMarmitas = document.getElementById("adicionar")
@@ -278,7 +147,7 @@ function listarMarmitasDoCombo() {
                     let conteudo = `
                     <li>
                 <label class="wrapper" for="">
-                  <input type="checkbox" name="${dado.nome}" />
+                  <input type="checkbox" limit="4" name="marmitas" required/>
                   <span>${dado.nome}</span>
                 </label>
               </li>`
@@ -286,5 +155,54 @@ function listarMarmitasDoCombo() {
                 todasAsMarmitas.innerHTML = todasAsMarmitas.innerHTML + conteudo;
             })
         }
-        );
+        ).then(function (){limitCheckbox()});
 }
+
+function limitCheckbox(){
+    let qtd = 4;
+    const checkboxes = document.querySelectorAll(`input[name="marmitas"]`);
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', () => {
+            const checkedCount = document.querySelectorAll(`input[name="marmitas"]:checked`).length;
+            if (checkedCount > qtd) {
+                checkbox.checked = false
+            }
+        });
+    });
+}
+
+
+function dadosDoCombo(){
+
+    let url = new URL(window.location.href);
+    
+    let idCombo = url.searchParams.get('idCombo');
+
+    fetch('http://localhost:8080/combos/' + idCombo).then(response => response.json()).
+        then(function (dados) {
+            let combo = document.getElementById("detalhes-do-combo")
+            combo.innerHTML = ""
+            let conteudo = `
+            <div class="apresentacao-da-comida" id="apresentacao-da-comida">
+          <img
+            src="${dados.img64}"
+            alt=""
+          />
+        </div>
+
+        <div class="box-apresentacao-do-prato" id="box-apresentacao-do-prato">
+          <div class="detalhes-do-prato" id="detalhes-do-prato">
+            <h3>
+            <strong>${dados.nome}</strong>
+          </h3>
+
+          <p>
+            ${dados.descricao}
+          </p>
+
+          <strong class="preco">${"R$" + dados.preco}</strong>
+          </div>
+        </div>`
+        combo.innerHTML = combo.innerHTML + conteudo;
+})}
